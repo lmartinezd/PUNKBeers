20645b5cd4cf4960135509eee7b6b3a891be7e2b
Luana <Luana@MacBook-Pro-de-Dante.local>
2017-11-26 22:53:47 -0200

    projeto finalizado IOS REST

---
+++ RM31894/Beer.swift
@@ -0,0 +1,9 @@
+//
+//  Beer.swift
+//  RM31894
+//
+//  Created by Luana on 25/11/17.
+//  Copyright © 2017 fiap. All rights reserved.
+//
+
+import Foundation
---
+++ RM31894/BeersTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BeersTableViewCell.swift
+//  RM31894
+//
+//  Created by Luana on 26/11/17.
+//  Copyright © 2017 fiap. All rights reserved.
+//
+
+import UIKit
+
+class BeersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
